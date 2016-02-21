1905c753a383a7ef4660089cb89d8131f1ee3df8
Tom Ritchford <tom@swirly.com>
2016-02-21 16:32:13 -0500

    First empty unit test.

--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <fcolor/inl.h>
-
-int main() {
-    auto c = fcolor::color(1, 2, 3);
-    auto d = fcolor::color(1, 2, 3, 0);
-    return c.green + d.red;
-}
---
+++ src/tests.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+
+#include <fcolor/inl.h>
+#include <catch/catch.hpp>
+
+TEST_CASE("hello", "world") {
+    REQUIRE(0 == 0);
+}
