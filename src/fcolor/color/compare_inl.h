7cec4fa17b12a92fa1ffe02b30671e9548178bc7
Tom Ritchford <tom@swirly.com>
2016-02-21 17:34:26 -0500

    More tests.

--- src/fcolor/color/compare_inl.h
+++ src/fcolor/color/compare_inl.h
@@ -1,20 +1,21 @@
+#pragma once
+
 #include <type_traits>
 #include <fcolor/color/compare.h>
-
-#pragma once
 
 namespace fcolor {
 
 template <
     typename Number,
-    typename std::enable_if<std::is_arithmetic<Number>::value, int> = 0
+    typename std::enable_if<std::is_arithmetic<Number>::value>::type* = nullptr
     >
+inline
 int compare(Number x, Number y) {
     return (x < y) ? -1 : (x < y) ? 1 : 0;
 }
 
 template <typename Number>
-int compare(Color<Number> const& x, Color<Number> const& y) {
+int compare(Color<Number> x, Color<Number> y) {
     if (auto c = compare(x.red, y.red))
         return c;
     if (auto c = compare(x.green, y.green))
@@ -25,7 +26,7 @@ int compare(Color<Number> const& x, Color<Number> const& y) {
 }
 
 template <typename Number, typename Alpha>
-int compare(Color<Number, Alpha> const& x, Color<Number, Alpha> const& y) {
+int compare(Color<Number, Alpha> x, Color<Number, Alpha> y) {
     if (auto c = compare(x.red, y.red))
         return c;
     if (auto c = compare(x.green, y.green))
@@ -37,34 +38,11 @@ int compare(Color<Number, Alpha> const& x, Color<Number, Alpha> const& y) {
     return 0;
 }
 
-template <typename T>
-bool operator==(T const& x, T const& y) {
-    return compare(x, y) == 0;
-}
-
-template <typename T>
-bool operator!=(T const& x, T const& y) {
-    return compare(x, y) != 0;
-}
-
-template <typename T>
-bool operator<(T const& x, T const& y) {
-    return compare(x, y) < 0;
-}
-
-template <typename T>
-bool operator<=(T const& x, T const& y) {
-    return compare(x, y) <= 0;
-}
-
-template <typename T>
-bool operator>(T const& x, T const& y) {
-    return compare(x, y) > 0;
-}
-
-template <typename T>
-bool operator>=(T const& x, T const& y) {
-    return compare(x, y) >= 0;
-}
+template <typename T> bool operator==(T x, T y) { return compare(x, y) == 0; }
+template <typename T> bool operator!=(T x, T y) { return compare(x, y) != 0; }
+template <typename T> bool operator<(T x, T y) { return compare(x, y) < 0; }
+template <typename T> bool operator<=(T x, T y) { return compare(x, y) <= 0; }
+template <typename T> bool operator>(T x, T y) { return compare(x, y) > 0; }
+template <typename T> bool operator>=(T x, T y) { return compare(x, y) >= 0; }
 
 }  // namespace fcolor
