4000f8fbaaf6aab17a937f23f30b85f4f1e64d5e
Tom Ritchford <tom@swirly.com>
2016-05-01 11:30:50 -0400

    Get rid of most old code.

--- src/tdsp/base/Rows.pyx
+++
@@ -1,5 +0,0 @@
-cdef extern from "tdsp2/Rows.h" namespace "tdsp":
-    int computeRows(int size, int columns)
-
-def compute_rows(int size, int columns):
-    return computeRows(size, columns)
--- src/tests.cpp
+++ src/tests.cpp
@@ -1,12 +1,8 @@
 #include <tdsp/color4/signal.h>
-#include <tdsp/color3/color.h>
-#include <tdsp/color3/strip.h>
 
 #define CATCH_CONFIG_MAIN
 
 #include <catch/catch.hpp>
-#include <tdsp/inl.h>
-#include <tdsp/color/color_test.cpp>
 
 TEST_CASE("hello", "world") {
     REQUIRE(0 == 0);
