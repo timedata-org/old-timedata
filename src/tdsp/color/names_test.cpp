fbf07b089c56c877c42eaa45eb17bbcea0f333fa
Tom Ritchford <tom@swirly.com>
2016-05-02 10:51:53 -0400

    Re-enabled broken test.

--- src/tdsp/color/names_test.cpp
+++ src/tdsp/color/names_test.cpp
@@ -1,9 +1,11 @@
+#pragma once
+
 #include <tdsp/color/names_inl.h>
 
 namespace tdsp {
 
 TEST_CASE("color", "names") {
-    REQUIRE(toString(Frame<RGB>{{0, 0, 0}}) == "black");
+   REQUIRE(toString(Frame<RGB>{{0, 0, 0}}) == "black");
 }
 
 } // namespace
