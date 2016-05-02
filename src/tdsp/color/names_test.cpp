6939f317be4bd7d5e6913578863dc43cb0c929f3
Tom Ritchford <tom@swirly.com>
2016-05-02 10:33:22 -0400

    New join routines.

--- src/tdsp/color/names_test.cpp
+++ src/tdsp/color/names_test.cpp
@@ -1,1 +1,9 @@
 #include <tdsp/color/names_inl.h>
+
+namespace tdsp {
+
+TEST_CASE("color", "names") {
+    REQUIRE(toString(Frame<RGB>{{0, 0, 0}}) == "black");
+}
+
+} // namespace
--- src/tests.cpp
+++ src/tests.cpp
@@ -3,7 +3,8 @@
 #define CATCH_CONFIG_MAIN
 
 #include <catch/catch.hpp>
-#include <tdsp/color/names_test.cpp>
+#include <tdsp/base/join_test.cpp>
+//#include <tdsp/color/names_test.cpp>
 
 TEST_CASE("hello", "world") {
     REQUIRE(0 == 0);
