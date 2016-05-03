1a094ef03d711f36452d3456f7f4b732a286ee84
Tom Ritchford <tom@swirly.com>
2016-05-03 15:39:43 -0400

    More magic methods for Color.

--- src/tdsp/color/color.pyx
+++ src/tdsp/color/color.pyx
@@ -41,19 +41,61 @@ cdef class Color:
             return self.blue
         raise IndexError('Color index out of range')
 
+    def __abs__(self): pass
+
+    def __add__(self, other): pass
+
+    def __div__(self, other): pass
+
+    def __divmod__(self, other): pass
+
+    def __floordiv__(self, other): pass
+
+    def __invert__(self): pass
+
     def __len__(self):
         return 3
 
-    def __str__(self):
-        return colorToString(self.red, self.green, self.blue).decode('ascii')
+    def __mod__(self, other): pass
+
+    def __mul__(self, other): pass
+
+    def __neg__(self): pass
+
+    def __pos__(self): pass
+
+    def __pow__(self, other, modulus): pass
+
+    def __radd__(self, other): pass
 
     def __repr__(self):
         cl = self.__class__
         return '%s.%s(%s)' % (cl.__module__, cl.__name__, str(self))
 
+    def __rdiv__(self, other): pass
+
+    def __rdivmod__(self, other): pass
+
+    def __rfloordiv__(self, other): pass
+
     def __richcmp__(Color self, Color c, int cmp):
         return cmpToRichcmp((self.red - c.red) or (self.green - c.green) or
                             (self.blue - c.blue), cmp)
+
+    def __rmod__(self, other): pass
+
+    def __rmul__(self, other): pass
+
+    def __round__(self, n): pass
+
+    def __rpow__(self, other, modulus): pass
+
+    def __rsub__(self, other): pass
+
+    def __str__(self):
+        return colorToString(self.red, self.green, self.blue).decode('ascii')
+
+    def __sub__(self, other): pass
 
     @staticmethod
     def make(x):
