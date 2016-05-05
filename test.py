2ca323082db4642cb45787c9de6f15f987c96606
Tom Ritchford <tom@swirly.com>
2016-05-05 10:57:08 -0400

    You can now slice colors.

--- test.py
+++ test.py
@@ -2,6 +2,11 @@ from tdsp.tdsp import Color, ColorList
 
 """Code to test the tdsp binary module."""
 
+white = Color(1, 1, 1)
+print(white[:])
+print(white[0:2])
+print(white[::-1])
+
 print(Color())
 print(Color(1, 1, 1))
 for i in range(1000):
