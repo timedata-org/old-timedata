7a75aa5bdddafc21d6a0a648bdaf476ba551a44c
Tom Ritchford <tom@swirly.com>
2016-05-07 11:12:12 -0400

    More on rendering.

--- src/tdsp/color/renderer.pyx
+++ src/tdsp/color/renderer.pyx
@@ -1,37 +1,45 @@
 cdef extern from "<tdsp/color/renderer.h>" namespace "tdsp":
     struct Render3:
-        float begin, end, brightness, gamma
+        float min, max, brightness, gamma
         uint perm
 
+        void apply(ColorList& input, char* message, uint offset)
+
+
 cdef class Renderer3:
-    cdef Render3 render
-    cdef uint size, offset
+    cdef Render3 _render
+    cdef uint _size, _offset
     cdef bytearray _message
 
-    def __cinit__(self, driver):
-        self.size = driver.size
-        self.offset = len(driver.header)
-        self.byte_size = 3 * self.size
+
+    def __cinit__(self, object driver):
+        self._size = driver.size
+        self._offset = len(driver.header)
+        self.byte_size = 3 * self._size
+
+    def render(self, _ColorList cl):
+        self._render.apply(cl.colors, self._message, self._offset)
 
     property message:
         def __get__(self):              return self._message
         def __set__(self, bytearray x): self._message = x
 
-    property begin:
-        def __get__(self):              return self.render.begin
-        def __set__(self, float x):     self.self.render.begin = x
+    property min:
+        def __get__(self):              return self._render.min
+        def __set__(self, float x):     self._render.min = x
 
-    property end:
-        def __get__(self):              return self.render.end
-        def __set__(self, float x):     self.self.render.end = x
+    property max:
+        def __get__(self):              return self._render.max
+        def __set__(self, float x):     self._render.max = x
 
     property brightness:
-        def __get__(self):              return self.render.brightness
-        def __set__(self, float x):     self.self.render.brightness = x
+        def __get__(self):              return self._render.brightness
+        def __set__(self, float x):     self._render.brightness = x
 
     property gamma:
-        def __get__(self):              return self.render.gamma
-        def __set__(self, float x):     self.self.render.gamma = x
+        def __get__(self):              return self._render.gamma
+        def __set__(self, float x):     self._render.gamma = x
 
-    def render(self):
-        pass
+    property perm:
+        def __get__(self):              return self._render.gamma
+        def __set__(self, float x):     self._render.gamma = x
