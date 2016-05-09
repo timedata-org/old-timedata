c9581b5cd9b3b160ae89287dfa76ccd7b4b6b43c
Tom Ritchford <tom@swirly.com>
2016-05-09 17:01:55 -0400

    combiner.pyx and stripe.pyx are now automatically generated.

--- src/tdsp/color/combiner.pyx
+++ src/tdsp/color/combiner.pyx
@@ -1,27 +1,42 @@
+# Automatically generated on 2016-05-09T21:03:55.580378
+# by https://github.com/rec/make_pyx/make_pyx.py
+
 cdef extern from "<tdsp/color/combiner.h>" namespace "tdsp":
     struct Combiner:
         float scale, offset
-        uint mute, invert
+        uint mute
+        uint invert
 
-
-cdef class _Combiner:
+cdef class _Combiner(_Wrapper):
     cdef Combiner _combiner;
 
+    def __str__(self):
+        return '(scale=%s, offset=%s, mute=%s, invert=%s)' % (
+            self.scale, self.offset, self.mute, self.invert)
+
     property scale:
-        def __get__(self):            return self._combiner.scale
-        def __set__(self, float x):   self._combiner.scale = x
+        def __get__(self):
+            return self._combiner.scale
+        def __set__(self, float x):
+            self._combiner.scale = x
 
     property offset:
-        def __get__(self):            return self._combiner.offset
-        def __set__(self, float x):   self._combiner.offset = x
+        def __get__(self):
+            return self._combiner.offset
+        def __set__(self, float x):
+            self._combiner.offset = x
 
     property mute:
-        def __get__(self):            return self._combiner.mute
-        def __set__(self, uint x):    self._combiner.mute = x
+        def __get__(self):
+            return self._combiner.mute
+        def __set__(self, uint x):
+            self._combiner.mute = x
 
     property invert:
-        def __get__(self):            return self._combiner.invert
-        def __set__(self, uint x):    self._combiner.invert = x
+        def __get__(self):
+            return self._combiner.invert
+        def __set__(self, uint x):
+            self._combiner.invert = x
 
 
-globals()['Color'] = _Color
+globals()['Combiner'] = _Combiner
