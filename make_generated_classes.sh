c17bac8cb455061070a4bcd13300df315b8d1104
Tom Ritchford <tom@swirly.com>
2016-05-10 18:57:33 -0400

    Improve display of enums in output.

--- make_generated_classes.sh
+++ make_generated_classes.sh
@@ -4,8 +4,7 @@ pushd src
 /development/make_pyx/make_pyx.py \
     tdsp/signal/fade.h \
     tdsp/signal/combiner.h \
-    tdsp/signal/permutation.h\
-    tdsp/signal/stripe.h\
     tdsp/signal/render3.h\
+    tdsp/signal/stripe.h\
 
 popd
