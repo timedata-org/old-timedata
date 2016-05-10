3ed27c3497a97116cc278a9589a8c1039364c7ab
Tom Ritchford <tom@swirly.com>
2016-05-10 17:21:49 -0400

    Minor changes.

--- src/tdsp/signal/render3.h
+++ src/tdsp/signal/render3.h
@@ -5,9 +5,22 @@
 namespace tdsp {
 
 struct Render3 {
+    enum class Permutation {rgb, rbg, grb, gbr, brg, bgr};
+
     float min = 0, max = 255, scale = 1, gamma = 1;
     uint8_t permutation = 0;
+    // Permutation permutation = Permutation::rgb;
     size_t offset = 0, size = 0;
+
+    float operator()(float s) const {
+        s = std::min(1.0f, std::max(0.0f, scale * s));
+        if (gamma != 1)
+            s = pow(s, gamma);
+        return std::min(max, min + (max - min + 1) * s);
+    }
+
+    using PermArray = std::array<uint8_t, 3>;
+    using PermsArray = EnumArray<PermArray, Permutation>;
 };
 
 } // tdsp
