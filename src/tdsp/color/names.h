57d36b5797f48ebacd91a103320bf71c4aaae4c7
Tom Ritchford <tom@swirly.com>
2016-05-11 14:47:24 -0400

    Split off color name tables.

--- src/tdsp/color/names.h
+++ src/tdsp/color/names.h
@@ -15,6 +15,14 @@ enum class Base {normal, integer};
 template <Base base = Base::normal>
 bool toColor(char const*, Color&);
 
+/** Convert a string to a Color.  Throws an exception on failure. */
+template <Base base = Base::normal>
+Color toColor(char const* name);
+
+/** Convert a Color to a string. */
+template <Base base = Base::normal>
+std::string toString(Color c);
+
 /** Convert a Color to a string. */
 template <Base base = Base::normal>
 std::string colorToString(float r, float g, float b);
