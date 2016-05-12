e47ac017bbf53c4b9f94e3e90979af5f4c8d337f
Tom Ritchford <tom@swirly.com>
2016-05-12 13:46:29 -0400

    Somewhat improved installation process.

--- tdsp/__init__.py
+++ tdsp/__init__.py
@@ -1,10 +1,1 @@
-from . tdsp import tdsp as _tdsp
-
-Color = _tdsp._Color
-Color256 = _tdsp._Color256
-ColorList = _tdsp._ColorList
-ColorList256 = _tdsp._ColorList256
-Combiner = _tdsp._Combiner
-Fade = _tdsp._FadeImpl
-Render3 = _tdsp._Render3Impl
-Stripe = _tdsp._Stripe
+from . tdsp import *
