4d6b7541f53ee93610a0a2cefad86f9668088cfa
Tom Ritchford <tom@swirly.com>
2016-05-13 15:32:07 -0400

    intermediate

--- src/tdsp/signal/operation.h
+++ src/tdsp/signal/operation.h
@@ -13,4 +13,39 @@ struct Operation {
     enum class NonCommutative {DIV, SUB, POW, size};
 };
 
+template <typename Collection>
+void operateInto(Operation::Unary, Collection&);
+
+template <typename Collection>
+void operateInto(Operation::Commutative, float x, Collection&);
+
+template <typename Collection>
+void operateInto(Operation::Commutative, Collection const& in, Collection&);
+
+
+template <typename Collection>
+void operateInto(Operation::NonCommutative, float x, Collection&);
+
+template <typename Collection>
+void operateInto(Operation::NonCommutative, Collection const& in, Collection&);
+
+
+template <typename Collection>
+void operateIntoLeft(Operation::NonCommutative, float x, Collection&);
+
+template <typename Collection>
+void operateIntoLeft(Operation::NonCommutative, Collection const& in, Collection&);
+
+
+template <typename Collection>
+Collection operate(Operation::Unary, Collection const&);
+
+template <typename Collection>
+Collection operate(Operation::Commutative,
+                   Collection const&, Collection const&);
+
+template <typename Collection>
+Collection operate(Operation::NonCommutative,
+                   Collection const&, Collection const&);
+
 } // tdsp
