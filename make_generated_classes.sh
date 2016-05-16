a9e655bcffab10249b9a4d239507f030817f3e57
Tom Ritchford <tom@swirly.com>
2016-05-16 12:51:00 -0400

    Now producing ColorList and ColorList256 from a Python template.

--- make_generated_classes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pushd src
-/development/make_pyx/make_pyx.py \
-    tdsp/signal/fade.h \
-    tdsp/signal/combiner.h \
-    tdsp/signal/stripe.h\
-
-popd
--- src/tdsp.pyx
+++ src/tdsp.pyx
@@ -10,7 +10,8 @@ include "tdsp/base/math.pyx"
 include "tdsp/base/wrapper.pyx"
 include "tdsp/color/color.pyx"
 include "tdsp/color/color_list_base.pyx"
-include "tdsp/color/color_list.template.pyx"
+include "tdsp/color/color_list.pyx"
+include "tdsp/color/color_list256.pyx"
 include "tdsp/signal/_combiner.pyx"
 include "tdsp/signal/_stripe.pyx"
 include "tdsp/signal/fade.pyx"
@@ -20,7 +21,7 @@ locals().update(
     Color=_Color,
     Color256=_Color256,
     ColorList=_ColorList,
-    #ColorList256=_ColorList256,
+    ColorList256=_ColorList256,
     Combiner=_Combiner,
     Fade=_FadeImpl,
     Render3=_Render3Impl,
