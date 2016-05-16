e96e96ce5fc0f76b8cf1bdb720a910b355c0ea27
Tom Ritchford <tom@swirly.com>
2016-05-15 21:10:46 -0400

    changes to benchmark

--- src/tdsp/color/speedtest.pyx
+++ src/tdsp/color/speedtest.pyx
@@ -1,25 +1,6 @@
 from numbers import Number
 
 cdef class OperatorTest:
-    """A list of RGB floating point Colors, with many mutating functions.
-
-       A ColorList looks quite like a Python list of Colors (which look like
-       tuples) with the big interface difference that operations like + and *
-       perform arithmetic and not list construction.
-
-       Written in C++, this class should consume significantly fewer memory and
-       CPU resources than a corresponding Python list, as well as providing a
-       range of useful facilities.
-
-       While ColorList provides a full set of functions and operations that
-       create new ColorLists, in each case there is a corresponding mutating
-       function or operation that works "in-place" with no heap allocations
-       at all, for best performance.
-
-       The base class ColorList is a list of Color, which are normalized to
-       [0, 1]; the derived class ColorList256 is a list of Color256, which
-       are normalized to [0, 255].
-"""
     cdef ColorList colors
 
     cdef _ColorList _make(self, object value=None):
@@ -40,11 +21,22 @@ cdef class OperatorTest:
             runInto(op, side, self._toColorList(x).colors, self.colors)
         return self
 
-    def operator_add(self, float x):
+    def operator_add(self, object x):
         """An operation that mutates this ColorList."""
         self._run_into(x, ADD)
 
-    def class_add(self, float x):
+    def operator_pow(self, object x):
+        """An operation that mutates this ColorList."""
+        self._run_into(x, POW)
+
+    def class_add(self, object x):
+        if isinstance(x, Number):
+            addInto(<float> x, self.colors)
+        else:
+            addInto(self._toColorList(x).colors, self.colors)
+        return self
+
+    def class_pow(self, object x):
         if isinstance(x, Number):
             addInto(<float> x, self.colors)
         else:
