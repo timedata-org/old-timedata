2b8d5273b41cbad3bf5bd77869e5432992a29381
Tom Ritchford <tom@swirly.com>
2016-05-16 10:12:29 -0400

    Split off common ColorList code.

--- src/tdsp.pyx
+++ src/tdsp.pyx
@@ -5,25 +5,13 @@ from libcpp.vector cimport vector
 ctypedef unsigned int uint
 ctypedef unsigned char uint8_t
 
-# TODO: these need to go somewhere much better.
-cdef extern from "<tdsp/base/math.h>" namespace "tdsp":
-    void clearStruct[T](T&)
 
-cdef extern from "<tdsp/color/operation.h>" namespace "tdsp":
-    cppclass ColorList:
-        Color& operator[](size_t)
-        size_t size()
-        void resize(size_t)
-        void clear();
-        bool fixKey(int& key)
-        void setColor(size_t, float r, float g, float b)
-        bool getColor(size_t , float r, float g, float b)
-
-
+include "tdsp/base/math.pyx"
 include "tdsp/base/wrapper.pyx"
 include "tdsp/color/color.pyx"
 include "tdsp/color/operation.pyx"
 #include "tdsp/color/correct_color_list.pyx"
+include "tdsp/color/color_list_base.pyx"
 include "tdsp/color/color_list.pyx"
 include "tdsp/color/operator_color_list.pyx"
 include "tdsp/color/speedtest.pyx"
