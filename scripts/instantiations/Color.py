5512cf4e717d792bb5bc800c1f0e2c4663a44542
Tom Ritchford <tom@swirly.com>
2016-06-12 12:00:45 -0400

    Extract out ColorBase.py.

--- scripts/instantiations/Color.py
+++ scripts/instantiations/Color.py
@@ -1,63 +1,11 @@
-begin = 0
+from . ColorBase import *
+
 class_cpp = 'ColorCpp'
 class_py = 'NewColor'
-compare = 'compare'
-from_string = 'stringToColor'
-to_string = 'colorToString'
-include_file = 'tada/color/cython_inl.h'
 output_file = 'src/tada/color/color.gen.pyx'
-properties = 'red', 'green', 'blue'
-namespace = 'tada::color'
-value_type = 'float'
 range = 1.0
-size = 3
-documentation = 'FIXME'
-fix_key = 'fixKey'
 
-methods = dict(
-    static=dict(
-        strings=(
-            dict(name_cpp='colorNames',
-                 name_py='names',
-                 documentation='Return a list all names for colors'),
-            ),
-        from_cpp=(
-            dict(name='from_hex',
-                 argument_type='uint32_t',
-                 documentation='Create a color from a hex integer'),
-            ),
-        ),
-
-    zero=dict(
-        return_cpp=(
-            dict(name='to_hex',
-                 return_type='uint32_t',
-                 documentation='Return the nearest hex for this color'),
-            ),
-        ),
-    )
-
-class_documentation = """A Color is an immutable RGB floating point color.
-
-       A Color looks very much like a Python tuple with three floating point
-       numbers, with the big exception that operators like + and * perform
-       arithmetic, not list-style operations.
-
-       The constructor for Color takes a triple of numbers but also strings
-       representing human-readable color names like "red" and "green".
-
-       Color supports the operations +, -, *, /, %, pow, and ~ (which means
-       "take the complementary color").
-
-       Colors are very lightweight - about half the size of a corresponding
-       Python tuple and more than twice as fast to create and copy.  Colors are
-       also immutable - all operations on Colors have to create a new Color.
-
-       There are two ranges for color components.  The base class Color is
-       normalized so that the standard range for output signals - that is,
-       outout color components - is [0.0, 1.0].  As always, intermediate signals
-       can be greater than 1 or even negative.
-
-       The derived class Color256 has a standard output range of [0, 255] for
-       better compatibility with existing RGB and DMX systems.
+class_documentation += """
+        NewColor is a normalized color class, with components having a
+        range of [0, 1]
     """
