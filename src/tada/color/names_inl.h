5cf7a7eae5859c975fe1bad958584496bbbad135
Tom Ritchford <tom@swirly.com>
2016-06-19 14:50:01 -0400

    Split off color/toString.h

--- src/tada/color/names_inl.h
+++ src/tada/color/names_inl.h
@@ -10,7 +10,7 @@
 #include <tada/base/enum.h>
 #include <tada/base/throw.h>
 #include <tada/base/math_inl.h>
-#include <tada/color/colorfulness.h>
+#include <tada/color/toString.h>
 #include <tada/color/names_table_inl.h>
 
 namespace tada {
@@ -53,60 +53,6 @@ inline float strtof(const char *nptr, char const **endptr) {
     auto r = ::strtof(nptr, &ep);
     *endptr = ep;
     return r;
-}
-
-template <typename Range>
-bool isNearHex(Ranged<Range> number) {
-    return isNearFraction(number, 255 / Range::RANGE);
-}
-
-template <typename Color>
-std::string addNegatives(Color const& c) {
-    std::string s;
-    auto r = (*c[0] < 0), g = (*c[1] < 0), b = (*c[2] < 0);
-    if (r or g or b) {
-        for (auto i: {r, g, b})
-            s += "+-"[i];
-    }
-    return s;
-};
-
-template <typename Color>
-uint32_t toHexNormal(Color c) {
-    uint32_t total = 0;
-    static uint32_t const MAX = 256;
-    for (auto i : c) {
-        total *= MAX;
-        auto x = MAX * std::abs(i.unscale());
-        total += std::min(MAX - 1, static_cast<uint32_t>(x));
-    }
-    return total;
-}
-
-template <typename Color>
-bool isGray(Color color) {
-    return colorfulness(color).unscale() < 0.0001;
-}
-
-template <typename Color>
-std::string toString(Color c) {
-    using Range = typename Color::range_t;
-    auto bounded = [](ValueType<Color> x) { return x.abs().inBand(); };
-    if (std::all_of(c.begin(), c.end(), bounded)) {
-        if (std::all_of(c.begin(), c.end(), isNearHex<Range>)) {
-            auto hex = toHexNormal(c);
-
-            auto i = colorMapInverse().find(hex);
-            if (i != colorMapInverse().end())
-                return i->second + addNegatives(c);
-        }
-
-        if (isGray(c)) {
-            auto gray = 100.0f * c[0].abs().unscale();
-            return "gray " + tada::toString(gray, 4) + addNegatives(c);
-        }
-    }
-    return commaSeparated(c, 7);
 }
 
 template <Base BASE>
