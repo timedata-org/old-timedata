e830f8accc8b47e931d31b04d751d97f60201669
Tom Ritchford <tom@swirly.com>
2016-07-02 01:18:45 -0400

    Remove unused template files.

--- src/pyx/timedata/template/one/take_number_return_class.pyx
+++
@@ -1,9 +0,0 @@
-### declare
-    C$classname $name(C$classname&, $number_type)
-
-### define
-    cpdef $name($classname self, $number_type x):
-        """$documentation"""
-        cdef $classname result = $classname()
-        result.cdata = $name(self.cdata, x)
-        return result
--- src/pyx/timedata/template/zero/return_class.pyx
+++
@@ -1,9 +0,0 @@
-### declare
-    C$classname $name(C$classname&)
-
-### define
-    cpdef $classname $name($classname self):
-        """$documentation"""
-        cdef $classname s = $classname()
-        s.cdata = $name(self.cdata)
-        return s
