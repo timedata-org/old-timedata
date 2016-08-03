6552a71e5a57460c7d6c6198f8672c98f710f5ab
Tom Ritchford <tom@swirly.com>
2016-08-03 11:36:48 -0400

    Add some Cython compiler directives.

--- timedata.pyx
+++ timedata.pyx
@@ -1,1 +1,7 @@
+# cython: embedsignature=True
+# cython: infer_types=True
+
+# TODO: perhaps include other compiler directives from here:
+# http://docs.cython.org/en/latest/src/reference/compilation.html
+
 include "src/pyx/timedata/timedata.pyx"
